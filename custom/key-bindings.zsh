c4df1ba08f9cf04e0d6f7f69145765d730a2d2fd
Caius Durling <dev@caius.name>
2010-02-23 21:07:25 +0000

    Adding sane bindings to up/down arrow to search history for current line.

---
+++ custom/key-bindings.zsh
@@ -0,0 +1,3 @@
+# make search up and down work, so partially type and hit up/down to find relevant stuff
+bindkey '^[[A' history-beginning-search-backward
+bindkey '^[[B' history-beginning-search-forward

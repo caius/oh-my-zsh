319831e644cd3e1e138ed1054e6b6dd65d3c58ad
Caius Durling <dev@caius.name>
2010-08-16 11:29:02 +0100

    Fix delete keys

--- custom/key-bindings.zsh
+++ custom/key-bindings.zsh
@@ -1,3 +1,8 @@
 # make search up and down work, so partially type and hit up/down to find relevant stuff
 bindkey '^[[A' history-beginning-search-backward
 bindkey '^[[B' history-beginning-search-forward
+
+# "Fix" delete
+bindkey "^[[3~" delete-char
+bindkey "^[3;5~" delete-char
+bindkey "^?" backward-delete-char
